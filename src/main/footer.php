63627b690915d4ef9fe6b0b309a70a5857622bb9
Justin Grant <jlgrock@hotmail.com>
2013-10-01 12:36:33 -0400

    fixing styles and php to better match this particular theme - going with purple/orange color scheme

--- src/main/footer.php
+++ src/main/footer.php
@@ -1,15 +1,12 @@
-		</div> <!-- end #content-area -->
-		
-		<div id="footer">
-a			<p id="copyright"><?php esc_html_e('Designed by ','Sky'); ?> <a href="http://www.elegantthemes.com" title="Elegant Themes">Elegant Themes</a> | <?php esc_html_e('Modifications By ','SkyChild'); ?> 
+    	</div> <!-- end #content -->
+    </div> <!-- end #container -->
+	<div id="footer">
+			<p id="copyright"><?php esc_html_e('Designed by ','Sky'); ?> <a href="http://www.elegantthemes.com" title="Elegant Themes">Elegant Themes</a> | <?php esc_html_e('Modifications By ','SkyChild'); ?> 
 			<script type="text/javascript">
 				document.write("<a href='&#109;&#97;&#105;&#108;&#116;&#111;: &#106;&#108;&#103;&#114&#111;&#99;&#107;&#64;&#104;&#111;&#116;&#109;&#97;&#105;&#108;&#46;&#99;&#111;&#109;'>Justin Grant</a>");
 			</script> <?php esc_html_e('Powered by ','Sky'); ?> <a href="http://www.wordpress.org">Wordpress</a></p>
 		</div> <!-- end #footer -->
-	</div> <!-- end #container -->
-	
-	<?php get_template_part('includes/scripts'); ?>
+
 	<?php wp_footer(); ?>
-	
 </body>
 </html>
